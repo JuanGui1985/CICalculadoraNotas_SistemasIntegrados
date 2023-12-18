391f42835b40c92d0eaa93f3221907cf67b3983c
JuanguiMuñoz <jgmj001@gmail.com>
2023-12-17 21:53:46 -0500

    Creación del controller y el service. Pendiente terminar.

---
+++ src/main/java/com/co/calculadoranotas/CiCalculadoraNotasApplication.java
@@ -0,0 +1,13 @@
+package com.co.calculadoranotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCalculadoraNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCalculadoraNotasApplication.class, args);
+	}
+
+}
