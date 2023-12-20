bebc733b014b120a6642b9ac23c1f5b1163424c5
JuanguiMuñoz <jgmj001@gmail.com>
2023-12-20 00:02:50 -0500

    Se culmina la lógica de la calculadora de notas, se finaliza el json de salida y se realizan pruebas

--- src/main/java/com/co/calculadoranotas/controller/CalcNotasController.java
+++ src/main/java/com/co/calculadoranotas/controller/CalcNotasController.java
@@ -1,10 +1,19 @@
+/** 
+ * IDE Version: Spring Tool Suite 4 V. 4.21.0
+ * jdk version: 17
+ *  Postman: V. 10.21
+ * */
+
 package com.co.calculadoranotas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.co.calculadoranotas.services.CalcNotasServices;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/calculadoranotas")
@@ -12,5 +21,10 @@ public class CalcNotasController {
 	
 	@Autowired
 	public CalcNotasServices services;
+	
+	@PostMapping(value = "/definitiva")
+	String mensajeDefinitiva(@PathParam(value = "nota1") double nota1, @PathParam(value = "nota2") double nota2, @PathParam(value = "nota3") double nota3) {
+		return services.mensajeDefinitiva(nota1, nota2, nota3);
+	}
 
 }
--- src/main/java/com/co/calculadoranotas/services/CalcNotasServices.java
+++ src/main/java/com/co/calculadoranotas/services/CalcNotasServices.java
@@ -1,8 +1,29 @@
+/** 
+ * IDE Version: Spring Tool Suite 4 V. 4.21.0
+ * jdk version: 17
+ *  Postman: V. 10.21
+ * */
+
 package com.co.calculadoranotas.services;
 
 import org.springframework.stereotype.Service;
+import java.text.DecimalFormat;
 
 @Service
 public class CalcNotasServices {
 
+	public String mensajeDefinitiva(double nota1, double nota2, double nota3) {
+		double definitiva = (nota1 + nota2 + nota3) / 3;
+		
+		DecimalFormat formatoDecimal = new DecimalFormat("#.##");
+		
+		String mensaje = null;
+			if(definitiva >= 3.0) {
+				mensaje = "Estado: Aprobado \nDefinitiva: " + formatoDecimal.format(definitiva);
+			}
+			else {
+				mensaje = "Estado: Reprobado \nDefinitiva: " + formatoDecimal.format(definitiva);
+			}
+		return mensaje;
+	}
 }
