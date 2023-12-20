61f2ddc7fa399f3e576b71f61823b4650e1bb03d
JuanguiMuñoz <jgmj001@gmail.com>
2023-12-20 00:32:09 -0500

    Se agregan los comentarios al código

--- src/main/java/com/co/calculadoranotas/controller/CalcNotasController.java
+++ src/main/java/com/co/calculadoranotas/controller/CalcNotasController.java
@@ -2,6 +2,11 @@
  * IDE Version: Spring Tool Suite 4 V. 4.21.0
  * jdk version: 17
  *  Postman: V. 10.21
+ *  DESARROLLADORES
+ *  Brayan Andrés Sepúlveda Garnica
+ *  Juan Camilo Benavidez Díaz
+ *  Fabián Hernando Cortes Jamaica
+ *  Juan Guillermo Muñoz Jaramillo
  * */
 
 package com.co.calculadoranotas.controller;
@@ -22,6 +27,7 @@ public class CalcNotasController {
 	@Autowired
 	public CalcNotasServices services;
 	
+	// Configuración del método POST y @PathParam, que permitirá capturar los parámetros desde la URL
 	@PostMapping(value = "/definitiva")
 	String mensajeDefinitiva(@PathParam(value = "nota1") double nota1, @PathParam(value = "nota2") double nota2, @PathParam(value = "nota3") double nota3) {
 		return services.mensajeDefinitiva(nota1, nota2, nota3);
--- src/main/java/com/co/calculadoranotas/services/CalcNotasServices.java
+++ src/main/java/com/co/calculadoranotas/services/CalcNotasServices.java
@@ -1,7 +1,12 @@
 /** 
  * IDE Version: Spring Tool Suite 4 V. 4.21.0
  * jdk version: 17
- *  Postman: V. 10.21
+ *  Postman: V.
+ *  DESARROLLADORES
+ *  Brayan Andrés Sepúlveda Garnica
+ *  Juan Camilo Benavidez Díaz
+ *  Fabián Hernando Cortes Jamaica
+ *  Juan Guillermo Muñoz Jaramillo
  * */
 
 package com.co.calculadoranotas.services;
@@ -12,11 +17,14 @@ import java.text.DecimalFormat;
 @Service
 public class CalcNotasServices {
 
+	// Declaración de la variable que llama el controlador para capturar los parámetros a calcular de la URL
 	public String mensajeDefinitiva(double nota1, double nota2, double nota3) {
 		double definitiva = (nota1 + nota2 + nota3) / 3;
 		
+		// Se crea la entidad formatoDecimal, con la cual se dará formato de dos decimales a la nota definitiva en la respuesta
 		DecimalFormat formatoDecimal = new DecimalFormat("#.##");
 		
+		// Se configura el mensaje que se mostrará en el JSON respuesta
 		String mensaje = null;
 			if(definitiva >= 3.0) {
 				mensaje = "Estado: Aprobado \nDefinitiva: " + formatoDecimal.format(definitiva);
